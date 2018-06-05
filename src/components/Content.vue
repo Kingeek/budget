05abf7f85577058d7cbd641a3a2cf1ced354cb52
Kingeek <kingeek@live.fr>
2018-06-05 23:28:57 +0200

    Add storage to Localstorage

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,6 +1,15 @@
 <template>
   <section class="content">
-    <Item />
+    <ul class="list-unstyled">
+      <Item v-for="item in items" :key="item.name" :data="item" />
+      <li class="item">
+        <div class="image"></div>
+        <div class="flex">
+          <input class="name" placeholder="name" v-model="Name" @keydown.enter="nextFocus">
+          <input class="value" placeholder="value" v-model="Value" @keydown.enter="addItem">
+        </div>
+      </li>
+    </ul>
   </section>  
 </template>
 
@@ -11,12 +20,90 @@ export default {
   name: "Content",
   components: {
     Item
+  },
+  data: function() {
+    return {
+      Name: "",
+      Value: "",
+      items: []
+    };
+  },
+  methods: {
+    addItem() {
+      this.items.push({ name: this.Name, value: this.Value });
+      this.Name = "";
+      this.Value = "";
+    }
+  },
+  mounted() {
+    if (localStorage.getItem("items"))
+      this.items = JSON.parse(localStorage.getItem("items"));
+  },
+  watch: {
+    items: {
+      handler() {
+        localStorage.setItem("items", JSON.stringify(this.items));
+      },
+      deep: true
+    }
   }
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .content {
   padding: 50px 40px;
+}
+
+.item {
+  padding: 10px;
+  border-radius: 5px;
+  transition: 0.15s ease;
+  max-width: 600px;
+
+  &:hover {
+    background-color: #fafafa;
+  }
+
+  .image {
+    width: 40px;
+    height: 40px;
+    display: inline-block;
+    vertical-align: middle;
+    border-radius: 5px;
+    background-color: #e7e7e7;
+    margin-right: 15px;
+  }
+
+  .flex {
+    display: inline-flex;
+    vertical-align: middle;
+    justify-content: space-between;
+    align-content: center;
+    align-items: center;
+    width: calc(100% - 70px);
+  }
+
+  .name {
+    font-size: 18px;
+    font-weight: 600;
+    display: inline-block;
+    vertical-align: middle;
+    border: none;
+    padding: 5px 10px;
+    outline: none;
+  }
+
+  .value {
+    font-size: 18px;
+    font-weight: 600;
+    display: inline-block;
+    vertical-align: middle;
+    border: none;
+    padding: 5px 10px;
+    outline: none;
+    text-align: right;
+    width: 100px;
+  }
 }
 </style>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="item">
     <div class="image">
-      <img src="" alt="">
+      <img :src="'//logo.clearbit.com/'+data.name+'.com'">
     </div>
-    <span class="name">Item title</span>
+    <div class="flex">
+      <span class="name">{{data.name}}</span>
+      <span class="value">{{data.value}}</span>
+    </div>
   </div>  
 </template>
 
 <script>
 export default {
-  name: "Item"
+  name: "Item",
+  props: {
+    data: {
+      type: Object
+    }
+  }
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .item {
+  padding: 10px;
+  border-radius: 5px;
+  transition: 0.15s ease;
+  max-width: 600px;
+
+  &:hover {
+    background-color: #fafafa;
+  }
+
   .image {
     width: 40px;
     height: 40px;
@@ -22,12 +39,39 @@ export default {
     vertical-align: middle;
     border-radius: 5px;
     background-color: #d1d1d1;
-    margin-right: 25px;
+    margin-right: 15px;
+
+    img {
+      object-fit: contain;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .flex {
+    display: inline-flex;
+    vertical-align: middle;
+    justify-content: space-between;
+    align-content: center;
+    align-items: center;
+    width: calc(100% - 70px);
   }
 
   .name {
     font-size: 18px;
     font-weight: 600;
+    display: inline-block;
+    vertical-align: middle;
+    padding: 5px 10px;
+  }
+
+  .value {
+    font-size: 18px;
+    font-weight: 600;
+    display: inline-block;
+    vertical-align: middle;
+    text-align: right;
+    padding: 5px 10px;
   }
 }
 </style>
